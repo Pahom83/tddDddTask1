c7800e5ee3ade5bf74ab1c85f7813e3b4e6eb812
Pahom83 <pol_83@mail.ru>
2023-09-05 12:47:13 +0400

    Stub and tests for add

--- src/main/java/netology/ru/Phonebook.java
+++ src/main/java/netology/ru/Phonebook.java
@@ -1,4 +1,7 @@
 package netology.ru;
 
 public class Phonebook {
+    public static int add (String name, int number){
+        return 0;
+    }
 }
--- src/test/java/netology/ru/PhonebookTest.java
+++ src/test/java/netology/ru/PhonebookTest.java
@@ -1,4 +1,21 @@
 package netology.ru;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhonebookTest {
+    Phonebook test = new Phonebook();
+
+    @Test
+    void addTest() {
+        int tested = test.add("Иван", 111111);
+        int tested2 = test.add("Иван", 222222);
+        int tested3 = test.add("Антон", 222222);
+        int expected = 1;
+        int expected2 = 2;
+
+        Assertions.assertEquals(expected, tested);
+        Assertions.assertEquals(expected, tested2);
+        Assertions.assertEquals(expected2, tested3);
+    }
 }
